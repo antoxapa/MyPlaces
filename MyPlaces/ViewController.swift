7b57314d7977379207bc6880c92660a6caa23271
Tony Po <antoxapa@gmail.com>
2020-01-05 09:47:50 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Антон Потапчик on 1/5/20.
+//  Copyright © 2020 TonyPo Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
