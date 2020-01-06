2ffb521f3aea2c89b2ce0cb4fb7bf1b37f284fcb
Tony Po <antoxapa@gmail.com>
2020-01-06 14:44:57 +0300

    Sorting button and segmented control

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -9,41 +9,47 @@
 import UIKit
 import RealmSwift
 
-class TableViewController: UITableViewController {
-
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var placesTableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
+    
     let cellID = "Cell"
-     
+    
     //  Same as Array in Realm
     var places: Results<Place>!
+    var ascendingSorting = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         places = realm.objects(Place.self)
     }
-
+    
     // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! CustomCell
-
+        
         let place = places[indexPath.row]
-
+        
         cell.placeName.text = place.name
         cell.placeType.text = place.type
         cell.placeLocation.text = place.location
         cell.placeImage.image = UIImage(data: place.imageData!)
-
+        
         cell.placeImage.layer.cornerRadius = cell.placeImage.frame.height / 2
-
+        
         return cell
     }
+    
     // MARK: - TableViewDelegate
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let place = places[indexPath.row]
         let contextItem = UIContextualAction(style: .destructive, title: "Delete") { (_, _ ,_)  in
             
@@ -54,22 +60,43 @@ class TableViewController: UITableViewController {
         return swipeActions
     }
     
-
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
-            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            guard let indexPath = placesTableView.indexPathForSelectedRow else { return }
             let place = places[indexPath.row]
             let dvc = segue.destination as! AddPlaceTableViewController
             dvc.currentPlace = place
         }
     }
-    
-    
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         guard let svc = segue.source as? AddPlaceTableViewController else { return }
         svc.savePlace()
-        tableView.reloadData()
+        placesTableView.reloadData()
     }
+    
+    // MARK: - Sorting
+    @IBAction func sortSelection(_ sender: UISegmentedControl) {
+        sorting()
+    }
+    @IBAction func reversedSorting(_ sender: UIBarButtonItem) {
+        ascendingSorting.toggle()
+        
+        if ascendingSorting {
+            reversedSortingButton.image = #imageLiteral(resourceName: "AZ")
+        } else {
+            reversedSortingButton.image = #imageLiteral(resourceName: "ZA")
+        }
+        sorting()
+    }
+    private func sorting() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            places = places.sorted(byKeyPath: "date", ascending: ascendingSorting)
+        } else {
+            places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
+        }
+        placesTableView.reloadData()
+    }
+    
 }
